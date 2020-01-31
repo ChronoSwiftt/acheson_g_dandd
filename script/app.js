4f08e9eaf87031dba1d8fd40c6d51f6ed25dbeb0
Acheson <g_acheson@ats-hr420-dm24.fc.ca>
2020-01-31 10:33:53 -0500

    getting piece and background to work

--- script/app.js
+++ script/app.js
@@ -1,13 +1,20 @@
 (() => {
 	// set up the puzzle pieces and boards
 	// navButtons -> images at the bottom of the page
-	let navButtons = document.querySelectorAll("buttonHolder img");
+	const navButtons = document.querySelectorAll("#buttonHolder img");
+		puzzlePiece = document.querySelectorAll('.puzzle-pieces img')
+		puzzleBoard = document.querySelector('.puzzle-board')
+
+// store the images names here
+const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
 // functions go here => what we want tot have happen when our triggers fire
 	function changeImageSet() {
 		//change the thumbnail images on the left to match the button images
+		pieces.forEach((piece, index) => puzzlePiece[index].src=`images/${piece + this.dataset.puzzleindex}.jpg`);
 		// and set a background image on the drop zone container
-		debugger;
+		//debugger;
+		puzzleBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleindex}.jpg)`;
 	}
 
 // add some event handling for the nav buttons
